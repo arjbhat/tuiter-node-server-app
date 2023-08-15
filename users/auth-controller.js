6227323dc82e644363c66c53ffb6ebbe266eda83
arjunbhat <arjun.bhat@cspi.com>
2023-08-16 00:08:20 +0530

    Finished

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -1,27 +1,48 @@
 import * as usersDao from "./users-dao.js";
 
 const AuthController = (app) => {
-  const register = (req, res) => {
-    const username = req.body.username;
-    const user = usersDao.findUserByUsername(username);
+  const register = async (req, res) => {
+    // const username = req.body.username;
+    // const user = usersDao.findUserByUsername(username);
+    // if (user) {
+    //   res.sendStatus(409);
+    //   return;
+    // }
+    // const newUser = usersDao.createUser(req.body);
+    // req.session["currentUser"] = newUser;
+    // res.json(newUser);
+    const user = await usersDao.findUserByUsername(req.body.username);
     if (user) {
-      res.sendStatus(409);
+      res.sendStatus(403);
       return;
     }
-    const newUser = usersDao.createUser(req.body);
+    const newUser = await usersDao.createUser(req.body);
     req.session["currentUser"] = newUser;
     res.json(newUser);
   };
 
-  const login = (req, res) => {
+  const login = async (req, res) => {
+    // const username = req.body.username;
+    // const password = req.body.password;
+    // const user = usersDao.findUserByCredentials(username, password);
+    // if (user) {
+    //   req.session["currentUser"] = user;
+    //   res.json(user);
+    // } else {
+    //   res.sendStatus(404);
+    // }
     const username = req.body.username;
     const password = req.body.password;
-    const user = usersDao.findUserByCredentials(username, password);
-    if (user) {
-      req.session["currentUser"] = user;
-      res.json(user);
+    if (username && password) {
+      const user = await usersDao.findUserByCredentials(username, password);
+      if (user) {
+        req.session["currentUser"] = user;
+        res.json(user);
+      } else {
+        res.sendStatus(403);
+      }
     } else {
-      res.sendStatus(404);
+      res.sendStatus(403);
     }
   };
 
@@ -39,21 +60,30 @@ const AuthController = (app) => {
     res.sendStatus(200);
   };
 
-  const getUsers = (req, res) => {
-    const users = usersDao.findAllUsers();
+  const getUsers = async (req, res) => {
+    const users = await usersDao.findAllUsers();
     res.json(users);
   };
 
-  const update = (req, res) => {
+  const update = async (req, res) => {
+    // const currentUser = req.session["currentUser"];
+    // if (!currentUser) {
+    //   res.sendStatus(404);
+    //   return;
+    // }
+    // const updates = req.body;
+    // const user = await usersDao.updateUser(currentUser._id, updates);
+    // req.session["currentUser"] = usersDao.findUserById(currentUser._id);
+    // res.json(req.session["currentUser"]);
+
     const currentUser = req.session["currentUser"];
     if (!currentUser) {
       res.sendStatus(404);
       return;
     }
-    const updates = req.body;
-    const user = usersDao.updateUser(currentUser._id, updates);
-    req.session["currentUser"] = usersDao.findUserById(currentUser._id);
-    res.json(req.session["currentUser"]);
+    const newUser = await usersDao.updateUser(currentUser._id, req.body);
+    req.session["currentUser"] = newUser;
+    res.json(newUser);
   };
 
   app.post("/api/users/register", register);
